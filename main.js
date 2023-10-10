3c302a12d62ec64023e9a40f83c3fbf668bacdfa
HikoMalik21 <hikmatmalikov31>
2023-10-10 18:30:14 +0400

    Task two JScript

---
+++ main.js
@@ -0,0 +1,93 @@
+//task one//
+
+var twoDigit = +prompt("Add any two digit number");
+while(twoDigit<100){
+    console.log(twoDigit);twoDigit+7;
+}
+
+// task two//
+
+
+
+
+// task three//
+var threedigit = 100;
+while(threedigit<1000){
+    if(threedigit % 10 ==0)
+    console.log(threedigit);threedigit+10;
+
+}
+
+//task four//
+
+var singleDigit = 11;
+while(singleDigit<100){
+    if((singleDigit % 2)-1)
+    console.log(singleDigit)   // esli ne budet rabotat.. dobav -1 suda//
+}
+
+//task five//
+
+
+
+
+//task six//
+
+let Nnumber = +prompt("Please write any number");
+while(Nnumber>0){ 
+    if(Nnumber % 2 == 0)
+    {console.log(Nnumber);}
+    Nnumber -= 3;
+
+    if(Nnumber % 2 !==0){
+        console.log(Nnumber)
+        Nnumber -= 2;
+    }
+}
+
+//task seven//
+
+for(var Num = 1; Num<10; )
+
+
+//task eight//
+
+var TextN = prompt("Please write sentence");
+var result = "";
+for (let i = TextN.length -1; )
+{
+    console.log(TextN[i])
+    result = result+TextN[1]
+    i--; 
+}
+
+var findNum = +prompt("Please write down a sentence which contains numbers");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
